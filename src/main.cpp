afbc1e1d8c1787dff6fb10cf2e5698bbd1ecef80
gui2one <guillaume.rouault.fx@gmail.com>
2021-04-11 22:14:01 +0200

    added central docking node

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,66 @@
 
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
+
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+void ImGUiInit(GLFWwindow *window)
+{
+    // init ImGui
+    // Setup Dear ImGui context
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    // io.Fonts->AddFontFromFileTTF(ORBITONS_RES_DIR "/fonts/JetBrainsMono-Regular.ttf", 16);
+
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    const char *glsl_version = "#version 130";
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ////////////
+    // end imgui config
+    ///////////
+}
+void ImGuiStartFrame()
+{
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::DockSpaceOverViewport(NULL, ImGuiDockNodeFlags_None | ImGuiDockNodeFlags_PassthruCentralNode /*|ImGuiDockNodeFlags_NoResize*/);
+}
+
+void ImGuiEndFrame()
+{
+
+    // Rendering
+    ImGui::Render();
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    ImGui::EndFrame();
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow *backup_current_context = glfwGetCurrentContext();
+
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+
+        glfwMakeContextCurrent(backup_current_context);
+    }
+}
 int main(int argc, char **argv)
 {
 
@@ -13,7 +73,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    GLFWwindow *window = glfwCreateWindow(640, 360, "Starter Project", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Starter Project", NULL, NULL);
 
     if (window == NULL)
     {
@@ -28,12 +88,24 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    ImGUiInit(window);
+
     glViewport(0, 0, 640, 360);
+    glfwSwapInterval(1);
     while (!glfwWindowShouldClose(window))
     {
+        ImGuiStartFrame();
 
-        glClearColor(1.f, 0.f, 0.f, 1.f);
+        glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        static bool showDemoWindow = true;
+        if (showDemoWindow)
+        {
+            ImGui::ShowDemoWindow(&showDemoWindow);
+        }
+
+        ImGuiEndFrame();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
